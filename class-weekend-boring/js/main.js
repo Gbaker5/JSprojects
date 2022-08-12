d443bb93563447e4bc08d7fb7c94903cc5c0e04f
Gjarred Baker <gjarred23@gmail.com>
2022-08-12 08:05:10 -0500

    finish gif backgrounds commit prev date

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -8,13 +8,16 @@ function check() {
 
   if(day === "tuesday" || day === "thursday"){
     document.querySelector('#placeToSee').innerText = "YOU HAVE CLASS"
+    document.querySelector('body').style.backgroundImage = 'url(img/nap-having-a-rest.gif)'
    
   }else if( day === "saturday" || day === "sunday"){
     document.querySelector('#placeToSee').innerText = "It's The Weekend"
+    document.querySelector('body').style.backgroundImage = 'url(img/tgif-weekend.gif)'
+    document.querySelector('body').style.backgroundSize = 'cover'
   }else{
     document.querySelector('#placeToSee').innerText = "BORING"
     document.querySelector('body').style.backgroundImage = 'url(img/bored.gif)'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    //document.querySelector('body').style.backgroundSize = 'cover'
   }
 
 }
