1aa3822d8eee49824a8af9830088786426898220
Gjarred Baker <gjarred23@gmail.com>
2022-09-09 08:05:10 -0500

    pseudo code commitiing on prev dat

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,1 +1,12 @@
 //Write your pseduo code first! 
+
+//make an event listener for button
+
+//take value from input
+
+//convert number using formula and hold in variable
+
+//display new conversion in dom 
+
+
+ 
