ec46e8a21c6077aa09fb91f85ab5bc0e25109c49
Gjarred Baker <gjarred23@gmail.com>
2022-09-11 08:05:10 -0500

    console log commiting on orev date

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,12 +1,18 @@
 //Write your pseduo code first! 
 
 //make an event listener for button
+document.querySelector('#F-Convert').addEventListener('click', FtoC)
+
+//create function
+function FtoC(){
 
 //take value from input
-
+initialD = Number(document.querySelector('#F-input').value)
+console.log(initialD)
 //convert number using formula and hold in variable
-
+convertedNum = (9*initialD)/5 +32
+console.log(convertedNum)
 //display new conversion in dom 
 
-
+}
  
