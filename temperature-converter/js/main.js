7c3bd22a88dcd57005151e085a39db839199469e
Gjarred Baker <gjarred23@gmail.com>
2022-09-13 08:05:10 -0500

    fixed first formula commit on prev date

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,5 +1,25 @@
 //Write your pseduo code first! 
 
+//C TO F CONVERSION
+
+//make an event listener for button
+document.querySelector('#C-Convert').addEventListener('click', CtoF)
+
+//create function
+function CtoF(){
+
+//take value from input
+initialD = Number(document.querySelector('#C-input').value)
+console.log(initialD)
+//convert number using formula and hold in variable
+convertedNum = (9*initialD)/5 +32
+console.log(convertedNum)
+//display new conversion in dom 
+document.querySelector('#Fahrenheit').innerText = `${convertedNum}\u00B0F`
+}
+ 
+//F TO C CONVERSION
+
 //make an event listener for button
 document.querySelector('#F-Convert').addEventListener('click', FtoC)
 
@@ -9,10 +29,11 @@ function FtoC(){
 //take value from input
 initialD = Number(document.querySelector('#F-input').value)
 console.log(initialD)
+
 //convert number using formula and hold in variable
-convertedNum = (9*initialD)/5 +32
+convertedNum = 
 console.log(convertedNum)
+
 //display new conversion in dom 
-document.querySelector('#Celsius').innerText = `${convertedNum}\u00B0`
+document.querySelector('#Fahrenheit').innerText = `${convertedNum}\u00B0C`
 }
- 
